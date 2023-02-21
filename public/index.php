67075a07683f65ae6c4332f73f04d3a1fcb5131e
marshmalocode <gustinmelvin6@gmail.com>
2023-02-21 12:49:01 +0100

    localhost

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+# Dependencies
+require_once "../config.php";# DB
+//require_once "../model/DOSSIER MODEL";# table post
+//require_once "../model/dOSSIER MODEL";# table category
+
+# Connexion
+try{
+    $db = mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
+    mysqli_set_charset($db,DB_CHARSET);
+}catch(Exception $e){
+    exit($e->getMessage());
+}
+# Router
+
+// connected controller
+if(false){
+    require_once "../controller/privateController.php";
+  
+// public
+}else{
+    require_once "../controller/publicController.php";
+}
+
+
+# good practice
+mysqli_close($db);
