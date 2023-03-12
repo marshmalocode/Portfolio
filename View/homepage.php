1e7338f83667afddea4cf4130aef380ac278e582
marshmalocodeuh <100229840+marshmalocode@users.noreply.github.com>
2023-03-12 15:31:08 +0100

    footer final

--- View/homepage.php
+++ View/homepage.php
@@ -6,7 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="asset/favicom.png" />
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/homePage.css">
+    <link rel="stylesheet" href="css/darkmode.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/menu.css">
     <script src="js/app.js" defer></script>
     <title><?= $titre ?></title>
 </head>
@@ -15,7 +18,7 @@
     <?php
     include '../View/inc/menu.php';
     ?>
-
+    <h1 class="homh1">titre</h1>
     <?php
     include '../View/inc/footer.php';
     ?>
--- View/inc/footer.php
+++ View/inc/footer.php
@@ -1,25 +1,36 @@
 <footer class="globalfooter">
     <div class="copyright">
-        <p>2020 © <a href="#" target="_blank">c'est copy right </a></p>
+        <p class="copyright-p">2020 © c'est copy right</p>
     </div>
     <div class="footer-lien">
         <nav class="footer-navigation">
             <ul class="footer-menu">
-                <li class="menu-item"><a href="?z=cv">CV</a></li>
-                <li class="menu-item"><a href="?z=trav">Travaux</a></li>
-                <li class="menu-item"><a href="?z=contact">Contact</a></li>
+                <div class="li-menu-item">
+                    <li class="menu-item"><a href="?z=cv" class="footer-menu-item">CV</a></li>
+                    <li class="menu-item"><a href="?z=trav" class="footer-menu-item">Travaux</a></li>
+                    <li class="menu-item"><a href="?z=contact" class="footer-menu-item">Contact</a></li>
+                </div>
             </ul>
         </nav>
     </div>
-    <button id="dark-mode-toggle" onclick="toggleDarkMode()">
-        <span id="toggle-icon" class="sun-icon"></span>
-        <span class="on-off-switch"></span>
-    </button>
+    <div class="dark">
+        <button id="dark-mode-toggle" onclick="toggleDarkMode()">
+            <span id="toggle-icon" class="sun-icon"></span>
+            <span class="on-off-switch"></span>
+        </button>
+    </div>
     <div class="footer-socials">
-        <a href="" class="icon-footer-socials"><img src="/asset/icon-github.png" alt=""></a>
-        <a href="" class="icon-footer-socials"><img src="/asset/icon-linkedin.png" alt=""></a>
-        <a href="" class="icon-footer-socials"><img src="/asset/icon-telegram.png" alt=""></a>
-        <a href="" class="icon-footer-socials"><img src="/asset/icon-insta.png" alt=""></a>
-        <a href="" class="icon-footer-socials"><img src="/asset/icon-user.png" alt=""></a>
+        <div class="footer-socials-ind">
+            <a href="" class="icon-footer-socials"><img class="icon-footer-socials-img" src="asset/icon-github.png" alt=""></a>
+        </div>
+        <div class="footer-socials-ind">
+            <a href="" class="icon-footer-socials"><img class="icon-footer-socials-img" src="asset/icon-linkedin.png" alt=""></a>
+        </div>
+        <div class="footer-socials-ind">
+            <a href="" class="icon-footer-socials"><img class="icon-footer-socials-img" src="asset/icon-insta.png" alt=""></a>
+        </div>
+        <div class="footer-socials-ind">
+            <a href="" class="icon-footer-socials"><img class="icon-footer-socials-img" src="asset/icon-user.png" alt=""></a>
+        </div>
     </div>
 </footer>
