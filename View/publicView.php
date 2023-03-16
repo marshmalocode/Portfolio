36290e3ee90dbf4c5af9c7b91ec1ed77fac07840
marshmalocodeuh <100229840+marshmalocode@users.noreply.github.com>
2023-03-16 19:58:01 +0100

    css connect

--- View/publicView.php
+++ View/publicView.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="asset/favicom.png" />
-    <link rel="stylesheet" href="css/homePage.css">
+    <link rel="stylesheet" href="css/public.css">
     <link rel="stylesheet" href="css/darkmode.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/menu.css">
@@ -19,16 +19,28 @@
     include '../View/inc/menu.php';
     ?>
     <div class="container">
-        <form name="connexion" action="" method="post">
-            <li class="dropdown-item">
-                <input name="username" placeholder="Votre nom d'utilisateur" required></input>
-            </li>
-            <li>
-                <input name="password" placeholder="Votre mot de passe" required></input>
-            </li>
-            <li>
-                <input type="submit" class="btn btn-primary"></input>
-        </form>
+        <div class="admin">
+            <div class="brand-logo">
+                <img src="asset/icon-user.png" alt="" class="logo-user">
+            </div>
+            <br>
+            <div class="brand-title">CONNEXION</div>
+            <form name="connexion" action="" method="post">
+                <ul>
+                    <li class="dropdown-item">
+                        <label class="label-item">EMAIL</label><br>
+                        <input class="script-item" name="username" placeholder="example@test.com" required></input>
+                    </li>
+                    <br>
+                    <li class="dropdown-item">
+                        <label class="label-item">PASSWORD</label><br>
+                        <input name="password" placeholder="Min 8 charaters long" required></input>
+                    </li>
+                    <li class="dropdown-item">
+                        <input type="submit" class="input-admin"></input>
+                    </li>
+            </form>
+        </div>
     </div>
     <?php
     include '../View/inc/footer.php';
